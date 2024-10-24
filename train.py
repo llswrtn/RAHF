b70cd86dd668b53258164aa5792512c533f52745
llswrtn <luisa.schwirten@gmail.com>
2024-10-24 16:28:08 +0200

    adabpt wandb logging, final base lr and warmup iterations

--- train.py
+++ train.py
@@ -22,10 +22,10 @@ metadata_dir = '/Users/luisa/RHF/RHF_parsed_dataset_with_captions/test'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 batch_size = 1
 num_epochs = 1
-#base_learning_rate = 0.015
-base_learning_rate = 1
+base_learning_rate = 0.015
+#base_learning_rate = 1
 
-wandb_project_name = 'rhf_test_project_0'
+wandb_project_name = 'rhf_test_project_0.1'
 
 wandb.init(
     # set the wandb project where this run will be logged
@@ -55,6 +55,42 @@ t5_text_encoder.eval()  # keep frozen? would that prevent catastrophic forgettin
 
 rahf_model = RAHFHeatmapModel(heatmap_predictor, t5_text_encoder, t5_encoder, vit_model)
 # Training Loop
+
+# Learning rate scheduler
+def lr_lambda(current_step):
+    warmup_steps = 2000
+    if current_step < warmup_steps:
+        return current_step / warmup_steps  # Linear warm-up
+    return (warmup_steps / current_step) ** 0.5  # Reciprocal square root decay
+
+
+# Load your dataset
+train_dataset = RHFDataset(images_dir, metadata_dir)
+
+# FOR TESTING ONLY, REMOVE NEXT 2 LINES AFTER TESTING
+subset_indices = list(range(20))  # Indices of the first 10 samples
+train_dataset = torch.utils.data.Subset(train_dataset, subset_indices)
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+
+criterion = nn.MSELoss()  # Assuming target heatmaps are continuous values
+
+''' 
+optimizer = optim.AdamW(list(heatmap_predictor.parameters()) +
+                        list(t5_encoder.parameters()) +
+                        list(vit_model.parameters()),
+                        lr=base_learning_rate)
+'''
+
+optimizer = optim.AdamW(rahf_model.parameters(), lr=base_learning_rate)
+
+
+
+# LambdaLR scheduler
+scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+
+
 def train(rahf_model, dataloader, criterion, optimizer, scheduler, device):
 
     rahf_model.to(device)
@@ -83,47 +119,23 @@ def train(rahf_model, dataloader, criterion, optimizer, scheduler, device):
         loss.backward()
         optimizer.step()
         lr_log = scheduler.get_last_lr()[0]
-        wandb.log({"loss": loss, "learning_rate":lr_log})
+        #wandb.log({"loss": loss, "learning_rate":lr_log})
         # Update the learning rate
         scheduler.step()
 
+        # Log metrics to Weights & Biases in real-time
+        wandb.log({
+            "epoch": epoch,
+            "train_loss": loss.item(),
+            "learning_rate": scheduler.get_last_lr()[0]
+        })
+
         print(f"Iteration {iteration}, Training Loss: {loss.item()}, Learning Rate: {lr_log}")
 
-
-# Load your dataset
-train_dataset = RHFDataset(images_dir, metadata_dir)
-
-# FOR TESTING ONLY, REMOVE NEXT 2 LINES AFTER TESTING
-subset_indices = list(range(20))  # Indices of the first 10 samples
-train_dataset = torch.utils.data.Subset(train_dataset, subset_indices)
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-
-criterion = nn.MSELoss()  # Assuming target heatmaps are continuous values
-
-''' 
-optimizer = optim.AdamW(list(heatmap_predictor.parameters()) +
-                        list(t5_encoder.parameters()) +
-                        list(vit_model.parameters()),
-                        lr=base_learning_rate)
-'''
-
-optimizer = optim.AdamW(rahf_model.parameters(), lr=base_learning_rate)
-
-# Learning rate scheduler
-def lr_lambda(current_step):
-    warmup_steps = 2
-    if current_step < warmup_steps:
-        return current_step / warmup_steps  # Linear warm-up
-    return (warmup_steps / current_step) ** 0.5  # Reciprocal square root decay
-
-# LambdaLR scheduler
-scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
 
 
 # Training Loop
 for epoch in range(num_epochs):
     print(f"Epoch {epoch+1}/{num_epochs}")
     train(rahf_model, train_loader, criterion, optimizer, scheduler, device)
-wandb.finish()
+#wandb.finish()
